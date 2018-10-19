b863c196fe6f923151167762c339433792e0273b
Jinho Huh <jh8579@gmail.com>
2018-10-20 08:28:14 +0900

    Change nginx.conf

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const app = express();
 
 app.get('/', (req, res) => {
 
-    res.send("dd")
+    // postgres 컨테이너에 app_db라는 도메인으로 접근 할수 있게 docker-compose에서 연결해준다.
+    const conString = "postgres://postgres:1234@app_db:5432/docker_tutorial";
+    const results = [];
+
+    var client = new pg.Client(conString);
+    client.connect(function (err) {
+        const results = [];
+        const query = client.query('SELECT * FROM car');
+
+        query.on('row', (row) => {
+            results.push(row);
+        });
+
+        query.on('end', () => {
+            client.end();
+            return res.json(results);
+        });
+    });
 });
 
 app.listen(3000, function () {
